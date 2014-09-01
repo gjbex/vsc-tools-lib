84259cc4e2d50d91811579ac491aad1f5b1f267c
Geert Jan BEX <geertjan.bex@uhasselt.be>
2014-09-01 07:44:14 +0200

    Added PbsJob class to represent parsing results

---
+++ src/vsc/pbs/job.py
@@ -0,0 +1,108 @@
+'''module to represent and manipulate PBS jobs'''
+
+class PbsJob(object):
+    '''Class representing a PBS job'''
+
+    def __init__(self):
+        '''Constructor for a PBS job object'''
+        self._name = None
+        self._resource_specs = {}
+        self._mail_specs = {'events': None, 'addresses': []}
+        self._queue = None
+        self._project = None
+        self._join = None
+        self._shebang = None
+        self._script = []
+
+    @property
+    def name(self):
+        '''returns the job's name, None if not set'''
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        '''Set the job's name'''
+        self._name = name
+
+    @property
+    def project(self):
+        '''returns the job's project name, None if not set'''
+        return self._project
+
+    @project.setter
+    def project(self, name):
+        '''Set the job's project name'''
+        self._project = name
+
+    @property
+    def queue(self):
+        '''returns the job's queue name, None if not set'''
+        return self._queue
+
+    @queue.setter
+    def queue(self, name):
+        '''Set the job's queue name'''
+        self._queue = name
+
+    @property
+    def resource_specs(self):
+        '''returns the job's resource specifications'''
+        return self._resource_specs
+
+    def add_resource_specs(self, resource_specs):
+        '''Add resources to specification'''
+        for key, value in resource_specs.items():
+            self._resource_specs[key] = value
+
+    @property
+    def mail_events(self):
+        '''return mail events'''
+        return self._mail_specs['events']
+
+    @mail_events.setter
+    def mail_events(self, events):
+        '''set mail events for job'''
+        self._mail_specs['events'] = events
+
+    @property
+    def mail_addresses(self):
+        '''return mail addresses'''
+        return self._mail_specs['addresses']
+
+    @mail_addresses.setter
+    def mail_addresses(self, addresses):
+        '''set mail address(es) for job mail events'''
+        if type(addresses) == list:
+            self._mail_specs['addresses'] = addresses
+        else:
+            self._mail_specs['addresses'] = [addresses]
+
+    @property
+    def join(self):
+        '''return I/O join'''
+        return self._join
+
+    @join.setter
+    def join(self, join):
+        '''set I/O join for job'''
+        self._join = join
+
+    @property
+    def shebang(self):
+        '''returns the job's shebang, None if not set'''
+        return self._shebang
+
+    @shebang.setter
+    def shebang(self, shebang):
+        '''Set the job's shebang'''
+        self._shebang = shebang
+
+    @property
+    def script(self):
+        '''returns the actual job script, i.e., the command to be executed'''
+        return '\n'.join([line[1] for line in self._script])
+
+    def add_script_line(self, line_nr, line):
+        '''adds a line to the script, retains line number information'''
+        self._script.append((line_nr, line))
+
