ed0e029b856ec1319679788da80398f8d48a2e9f
gjbex <geertjan.bex@uhasselt.be>
2014-10-09 13:59:07 +0200

    Fixed bug in JobStatus __str__  method, added attributes for account and holds

--- src/vsc/moab/job.py
+++ src/vsc/moab/job.py
@@ -21,6 +21,8 @@ class JobStatus(object):
         self._procs = procs
         self._time = time
         self._date = date
+        self._account = None
+        self._holds = []
 
     @property
     def id(self):
@@ -74,12 +76,32 @@ class JobStatus(object):
         else:
             raise InconsistantAttributesError('running job has no queue time')
 
+    @property
+    def account(self):
+        '''Return the account specified for charging, if any'''
+        return self._account
+
+    @account.setter
+    def account(self, account):
+        '''Set the account specified for charging'''
+        self._account = account
+
+    @property
+    def holds(self):
+        '''Returns list of holds in place on job, may be empty if no
+           holds are in place'''
+        return self._holds
+
+    def add_hold(self, hold):
+        '''Add a hold to the job status'''
+        self._holds.append(hold)
+
     def __str__(self):
         '''Returns string representation of a job status'''
         job_str = 'ID {0}:'.format(self.id)
         job_str += '\n\tuser: {0}'.format(self.username)
         job_str += '\n\tstate: {0}'.format(self.state)
-        job_str += '\n\tprocs: {0}'.format(self.state)
+        job_str += '\n\tprocs: {0}'.format(self.procs)
         if self.state == 'Running':
             job_str += '\n\tremaining: {0}'.format(self.remaining)
         else:
