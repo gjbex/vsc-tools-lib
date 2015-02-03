5fd192818b923c65d9a4a05050098c6c09af0e6c
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-02-03 19:03:07 +0100

    Added unit test for PBS script parser

---
+++ src/test/script_parser_test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+'''module to test the vsc.pbs.script_parser.PbsScriptParser parser'''
+
+import unittest
+from vsc.pbs.script_parser import PbsScriptParser
+
+class PbsScriptParserTest(unittest.TestCase):
+    '''Tests for the pbsnodes output parser'''
+
+    def test_parsing(self):
+        file_name = 'data/correct.pbs'
+        name = 'my_job'
+        project = 'lp_qlint'
+        nodes = 2
+        ppn = 4
+        walltime = 72*3600
+        qos = 'debugging'
+        join = 'oe'
+        parser = PbsScriptParser()
+        with open(file_name, 'r') as pbs_file:
+            parser.parse_file(pbs_file)
+        job = parser.job
+        self.assertEquals(name, job.name)
+        self.assertEquals(project, job.project)
+        nodes_specs = job.resource_specs['nodes'][0]
+        self.assertEquals(nodes, nodes_specs['nodes'])
+        self.assertEquals(ppn, nodes_specs['ppn'])
+        self.assertEquals(walltime, job.resource_specs['walltime'])
+        self.assertEquals(qos, job.resource_specs['qos'])
+        self.assertEquals(join, job.join)
