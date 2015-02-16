fae2bd2f20221449fba92af04dd5d4ce4c94a749
Geert Jan BEX <geertjan.bex@uhasselt.be>
2015-02-16 11:31:49 +0100

    Added logic and configuration info to compute cost of a job

---
+++ lib/vsc/mam/quote.py
@@ -0,0 +1,50 @@
+'''module containing logic to compute the credit cost of a job'''
+
+import sqlite3
+
+class QuoteCalculator(object):
+    '''class implementing a calculator of job costs'''
+
+    def __init__(self, config):
+        '''constructor that takes a configuration'''
+        self._config = config
+
+    def determine_node_types(self, resource_spec, partition):
+        '''determine the relevant type of a node to base to cost
+           calculation on'''
+        node_types = resource_spec['features']
+        if not node_types:
+            with sqlite3.connect(self._config['cluster_db']) as conn:
+                cursor = conn.cursor()
+                result = cursor.execute(
+                    '''SELECT DISTINCT p.property    
+                           FROM properties as p, nodes as n,
+                                partitions as p
+                           WHERE p.node_id = n.node_id AND
+                                 n.partition_id = p.partition_id AND
+                                 p.partition_name = ?''',
+                    (partition, ))
+                for row in result:
+                    node_types.append(row[0])
+        return node_types
+        
+    def compute(self, job):
+        '''compute the cost in credits of the given job'''
+        type_costs = self._config['node_type_credits']
+        walltime = float(job.resource_specs['walltime'])
+        node_rate = self._config['node_rate']
+        partition = job.resource_specs['partition']
+        resource_specs = job.resource_specs['nodes']
+        cost = 0.0
+        for resource_spec in resource_specs:
+            nr_nodes = resource_spec['nodes']
+            node_types = self.determine_node_types(resource_spec, partition)
+            node_cost = -1.0
+            for node_type in node_types:
+                if node_type in type_costs:
+                    if type_costs[node_type] > node_cost:
+                        node_cost = type_costs[node_type]
+            if node_cost < 0.0:
+                node_cost = type_costs[job.resource_specs['partition']]
+            cost += nr_nodes*node_cost
+        return cost*walltime*node_rate
---
+++ tests/test/quote_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+'''module to test the vsc.mam.quote.QuoteCalculator'''
+
+import json, unittest
+from vsc.pbs.script_parser import PbsScriptParser
+from vsc.mam.quote import QuoteCalculator
+
+class QuoteCalculatorTest(unittest.TestCase):
+    '''Tests the computations of job costs'''
+
+    def setUp(self):
+        conf_file_name = '../../conf/config.json'
+        event_file_name = '../../lib/events.json'
+        with open(conf_file_name, 'r') as conf_file:
+            self._config = json.load(conf_file)
+        self._config['cluster_db'] = 'data/cluster.db'
+        self._config['mock_balance'] = 'data/gbalance_new.txt'
+        with open(event_file_name, 'r') as event_file:
+            self._event_defs = json.load(event_file)
+
+    def test_compute(self):
+        file_name = 'data/correct.pbs'
+        expected_cost = 685.99
+        parser = PbsScriptParser(self._config, self._event_defs)
+        with open(file_name, 'r') as pbs_file:
+            parser.parse_file(pbs_file)
+        quote_calc = QuoteCalculator(self._config)
+        credits = quote_calc.compute(parser.job)
+        self.assertAlmostEqual(expected_cost, credits, delta=0.1)
