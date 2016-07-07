0f7c04d4da65a5b37c32b1734db46e70772d157a
gjbex <geertjan.bex@uhasselt.be>
2016-07-07 16:56:53 +0200

    script should now work on the cluster as well as locally

--- notebooks/start_notebook.sh
+++ notebooks/start_notebook.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-module load conda
-source activate py2k
+case ${VSC_INSTITUTE_CLUSTER} in
+    thinking)
+        source $VSC_DATA/miniconda2/env.sh
+        source activate admin
+        ;;
+    *)
+        module load conda
+        source activate py2k
+        ;;
+esac
+
 
 export PYTHONPATH="../lib:${PYTHONPATH}"
 jupyter notebook
