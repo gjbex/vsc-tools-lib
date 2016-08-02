0f330b0d53c80ac1e70139e09c8b5721a7fbc2ce
gjbex <geertjan.bex@uhasselt.be>
2016-08-02 15:30:07 +0200

    Added walltime to output

--- bin/job_summary.py
+++ bin/job_summary.py
@@ -41,6 +41,12 @@ def get_cpu_time(node, curr_job_id):
     return None
 
 
+def get_walltime(node, curr_job_id):
+    for job_id, job_info in node.status['job_info'].iteritems():
+        if job_id.startswith(curr_job_id):
+            return job_info['walltime']
+    return None
+
 if __name__ == '__main__':
     arg_parser = ArgumentParser(description='print summary statistics '
                                             'for a running job')
@@ -63,6 +69,7 @@ if __name__ == '__main__':
     mems = []
     netloads = []
     cput = None
+    walltime = None
     node_names = []
     for node in nodes:
         if job_id in node.job_ids:
@@ -70,12 +77,18 @@ if __name__ == '__main__':
             mems.append(compute_memory(node))
             netloads.append(float(node.status['netload']))
             cpu_time = get_cpu_time(node, job_id)
+            wall_time = get_walltime(node, job_id)
             if cpu_time:
                 cput = cpu_time
+            if wall_time:
+                walltime = wall_time
             node_names.append(node.hostname)
     if loadaves:
         print 'nodes = {0}'.format(len(loadaves))
-        print 'cpu time: {0}'.format(seconds2walltime(cput))
+        if cput:
+            print 'cpu time: {0}'.format(seconds2walltime(cput))
+        if walltime:
+            print 'walltime: {0}'.format(seconds2walltime(walltime))
         print 'loadave:'
         stats = compute_stats(loadaves)
         fmt_str = '  min: {0:.1f}, median: {1:.1f}, max: {2:.1f}'
